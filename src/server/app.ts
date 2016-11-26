bba2f4b8351291d4eb449471c3cc49eda03db0d5
Varun Jayaraman <varun.jayaraman@gmail.com>
2016-11-25 20:07:41 -0500

    fix export in app

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,24 +1,12 @@
 import * as express from 'express';
 
+export function app(injector) {
+  const app = express();
 
-export class App {
-  constructor(private app = express()) {
-  }
-
-  public init(injector) {
-    this.app.use('/', (req: express.Request, res: express.Response) => {
-      res.json({ hello: 'hi' })
-    });
-  }
+  
+  app.use('/', (req: express.Request, res: express.Response) => {
+    res.json({ hello: 'hi' });
+  });
+  
+  return app;
 }
-
-// export function app(injector) {
-//   const app = express();
-
-  
-//   app.use('/', (req: express.Request, res: express.Response) => {
-//     res.json({ hello: 'hi' });
-//   });
-  
-//   return app;
-// }
