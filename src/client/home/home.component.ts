91903e03e2e693aeeb81693c6a9d16cf67586030
Varun Jayaraman <varun.jayaraman@gmail.com>
2016-11-28 01:40:57 -0500

    begin styling

--- src/client/home/home.component.ts
+++ src/client/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewReadyService } from 'client/shared/view-ready.service';
 
 @Component({
   selector: 'home',
-  templateUrl: './home.jade'
+  templateUrl: './home.jade',
+  styleUrls: ['./home.styl']
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private viewState: ViewReadyService) { }
+  ngOnInit() {
+    this.viewState.emitFinished();
+  }
 }
