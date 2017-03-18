f896a029651c07483222aff91042a10245b19f37
Varun Jayaraman <varun.jayaraman@gmail.com>
2017-03-18 11:27:54 -0400

    feat: begin adding links to sidenav

--- src/client/sidebar/sidebar.component.ts
+++ src/client/sidebar/sidebar.component.ts
@@ -1,7 +1,7 @@
 // NPM Deps
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Subscription } from 'rxjs/Subscription';
 // Our Deps
 import { UserService } from 'client/core/user.service';
 
@@ -12,10 +12,30 @@ import { UserService } from 'client/core/user.service';
 })
 
 
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
   NAV_SELECTOR: string = `#aom-sidebar`;
+  user: any;
+
+  subscriptions: {
+    userState?: Subscription;
+  } = { userState: undefined };
 
   constructor(private userService: UserService, private router: Router) { }
+
+  ngOnInit() {
+    console.log(this.userService);
+    this.subscriptions.userState = this.userService.state$.subscribe(
+      (val: string) => {
+        if (val === 'LOGGED_IN') {
+          this.user = this.userService.$;
+        }
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.userState.unsubscribe();
+  }
 
   public logout = () => {
     this.userService.logout();
--- src/client/sidebar/sidebar.module.ts
+++ src/client/sidebar/sidebar.module.ts
@@ -1,11 +1,13 @@
 // NPM Deps
 import { NgModule } from '@angular/core'
-
+import { RouterModule } from '@angular/router';
 // Our Deps
 import { SidebarComponent } from './sidebar.component';
+import { SidebarLinksService } from './sidebar-links.service';
 import { SidebarToggleComponent } from './sidebar-toggle.component';
 
 @NgModule({
+  imports: [RouterModule],
   declarations: [SidebarComponent, SidebarToggleComponent],
   exports: [SidebarComponent, SidebarToggleComponent]
 })
