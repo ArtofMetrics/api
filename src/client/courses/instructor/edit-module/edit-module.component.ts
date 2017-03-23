f982d0313b38e5e18af19b6c3ae3a6603de8ce38
Varun Jayaraman <varun.jayaraman@gmail.com>
2017-03-23 00:25:57 -0400

    chore: add ViewReadyservice

--- src/client/courses/instructor/edit-module/edit-module.component.ts
+++ src/client/courses/instructor/edit-module/edit-module.component.ts
@@ -1,13 +1,26 @@
 // External Deps
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// AOM Deps
+import { ApiService } from 'client/core/api/api.service';
+import { ViewReadyService } from 'client/shared/view-ready.service';
 
 @Component({
   selector: 'edit-module',
   templateUrl: './edit-module.component.jade'
 })
 
-export class EditModuleComponent {
-  constructor() {
+export class EditModuleComponent implements OnInit {
+  constructor(
+    private apiService: ApiService,
+    private viewState: ViewReadyService
+  ) {}
+
+  ngOnInit() {
+
+  }
+
+  getCourse() {
     
   }
 }
