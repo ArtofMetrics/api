6dce12a8c6947268c1e807f278ab2b3dbc327339
Varun Jayaraman <varun.jayaraman@gmail.com>
2017-03-27 22:59:44 -0400

    feat: add EditModuleModule

--- src/client/courses/instructor/edit-module/edit-module.module.ts
+++ src/client/courses/instructor/edit-module/edit-module.module.ts
@@ -2,9 +2,11 @@
 import { NgModule } from '@angular/core';
 
 // AOM Deps
+import { EditModuleComponent } from './edit-module.component';
 
 @NgModule({
-
+  exports: [EditModuleComponent],
+  declarations: [EditModuleComponent]
 })
 
 export class EditModuleModule { }
