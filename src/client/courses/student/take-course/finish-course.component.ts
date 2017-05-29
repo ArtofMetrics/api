77970a83e7013f416805bc727582281961d3207d
Varun Jayaraman <varun.jayaraman@gmail.com>
2017-05-29 19:41:56 -0400

    reset course

--- src/client/courses/student/take-course/finish-course.component.ts
+++ src/client/courses/student/take-course/finish-course.component.ts
@@ -1,10 +1,14 @@
 // External Dependencies
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 // AOM Dependencies
 import { ViewReadyService } from 'client/shared/view-ready.service';
+import { ApiService } from 'client/core/api/api.service';
+import { ErrorService } from 'client/core/error.service';
 
 // AOM interfaces
+import { StudentCourse } from 'server/dependencies/models/course/student-course';
 
 @Component({
   selector: 'finish-course',
@@ -12,11 +16,44 @@ import { ViewReadyService } from 'client/shared/view-ready.service';
 })
 
 export class FinishCourseComponent implements OnInit {
+  studentCourse: StudentCourse;
+
   constructor(
-    private viewState: ViewReadyService
+    private viewState: ViewReadyService,
+    private apiService: ApiService,
+    private routeParams: ActivatedRoute,
+    private errorService: ErrorService,
+    private router: Router
   ) {}
 
   ngOnInit() {
-    this.viewState.emitFinished();
+    this.routeParams.params
+      .subscribe(
+        (data: { slug: string }) => {
+          this.apiService.students.getCourseBySlug({ slug: data.slug })
+            .subscribe(
+              data => {
+                if (!data.course.isCompleted) {
+                  return this.router.navigate(['course', data.course.slug]);
+                }
+
+                this.studentCourse = data.course;
+                this.viewState.emitFinished();
+              },
+              error => this.handleHttpError(error)
+            );
+        },
+        error => {
+          console.error(error);
+        }
+      );
   }
+
+  handleHttpError = (error: Error) => {
+    this.errorService.handleHttpError(error);
+  }
+
+  resetCourse = () => {
+    this.apiService.students.resetCourse({ course: this.studentCourse });
+  };
 }
--- src/server/api/students/index.ts
+++ src/server/api/students/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 
 // AOM Dependencies
 import { Middleware } from '../middleware';
-import { changeActiveLanguage, getCourses, getOneCourse, subscribeToCourse, submitDrip } from './routes';
+import { resetCourse, changeActiveLanguage, getCourses, getOneCourse, subscribeToCourse, submitDrip } from './routes';
 
 export function studentsRouter(di): Router {
   const api = Router();
@@ -23,6 +23,8 @@ export function studentsRouter(di): Router {
 
   api.route('/course/:identifier/language')
     .post(di.invoke(changeActiveLanguage));
-
+  
+  api.route('/course/:identifier/reset')
+    .post(di.invoke(resetCourse));
   return api;
 }
